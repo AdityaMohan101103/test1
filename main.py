78bfd3a1ccfaab63bf89537d71bafcd0d1c9cfa2
AdityaMohan101103 <157048346+AdityaMohan101103@users.noreply.github.com>
2025-05-29 12:38:11 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+# main.py
+from fastapi import FastAPI, Query
+from fastapi.middleware.cors import CORSMiddleware
+from scraper import get_menu
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["GET"],
+    allow_headers=["*"]
+)
+
+@app.get("/scrape")
+def scrape(url: str = Query(...)):
+    try:
+        data = get_menu(url)
+        return {"status": "success", "items": data}
+    except Exception as e:
+        return {"status": "error", "message": str(e)}
